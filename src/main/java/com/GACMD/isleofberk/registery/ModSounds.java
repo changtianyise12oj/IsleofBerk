415133a70a12e9cffe16249daee4524449e9fc64
GhanouCraft <nevapdm07022003@gmail.com>
2023-01-08 15:02:18 +0200

    Sounds almost done

--- src/main/java/com/GACMD/isleofberk/registery/ModSounds.java
+++ src/main/java/com/GACMD/isleofberk/registery/ModSounds.java
@@ -18,6 +18,7 @@ public class ModSounds {
     public static final RegistryObject<SoundEvent> NIGHT_FURY_TAME                  = registerSoundEvent("entity.night_fury.tame");
     public static final RegistryObject<SoundEvent> NIGHT_FURY_SLEEP                 = registerSoundEvent("entity.night_fury.sleep");
     public static final RegistryObject<SoundEvent> NIGHT_FURY_FIRE                  = registerSoundEvent("entity.night_fury.fire");
+    public static final RegistryObject<SoundEvent> NIGHT_FURY_BITE                  = registerSoundEvent("entity.night_fury.bite");
 
     public static final RegistryObject<SoundEvent> LIGHT_FURY_GROWL                 = registerSoundEvent("entity.light_fury.growl");
     public static final RegistryObject<SoundEvent> LIGHT_FURY_HURT                  = registerSoundEvent("entity.light_fury.hurt");
@@ -25,6 +26,7 @@ public class ModSounds {
     public static final RegistryObject<SoundEvent> LIGHT_FURY_TAME                  = registerSoundEvent("entity.light_fury.tame");
     public static final RegistryObject<SoundEvent> LIGHT_FURY_SLEEP                 = registerSoundEvent("entity.light_fury.sleep");
     public static final RegistryObject<SoundEvent> LIGHT_FURY_FIRE                  = registerSoundEvent("entity.light_fury.fire");
+    public static final RegistryObject<SoundEvent> LIGHT_FURY_BITE                  = registerSoundEvent("entity.light_fury.bite");
 
     public static final RegistryObject<SoundEvent> TRIPLE_STRYKE_GROWL              = registerSoundEvent("entity.triple_stryke.growl");
     public static final RegistryObject<SoundEvent> TRIPLE_STRYKE_HURT               = registerSoundEvent("entity.triple_stryke.hurt");
@@ -40,6 +42,7 @@ public class ModSounds {
     public static final RegistryObject<SoundEvent> DEADLY_NADDER_TAME               = registerSoundEvent("entity.deadly_nadder.tame");
     public static final RegistryObject<SoundEvent> DEADLY_NADDER_SLEEP              = registerSoundEvent("entity.deadly_nadder.sleep");
     public static final RegistryObject<SoundEvent> DEADLY_NADDER_FIRE               = registerSoundEvent("entity.deadly_nadder.fire");
+    public static final RegistryObject<SoundEvent> DEADLY_NADDER_BITE               = registerSoundEvent("entity.deadly_nadder.bite");
 
     public static final RegistryObject<SoundEvent> GRONCKLE_GROWL                   = registerSoundEvent("entity.gronckle.growl");
     public static final RegistryObject<SoundEvent> GRONCKLE_HURT                    = registerSoundEvent("entity.gronckle.hurt");
@@ -55,6 +58,7 @@ public class ModSounds {
     public static final RegistryObject<SoundEvent> MONSTROUS_NIGHTMARE_TAME         = registerSoundEvent("entity.monstrous_nightmare.tame");
     public static final RegistryObject<SoundEvent> MONSTROUS_NIGHTMARE_SLEEP        = registerSoundEvent("entity.monstrous_nightmare.sleep");
     public static final RegistryObject<SoundEvent> MONSTROUS_NIGHTMARE_FIRE         = registerSoundEvent("entity.monstrous_nightmare.fire");
+    public static final RegistryObject<SoundEvent> MONSTROUS_NIGHTMARE_BITE         = registerSoundEvent("entity.monstrous_nightmare.bite");
 
     public static final RegistryObject<SoundEvent> HIDEOUS_ZIPPLEBACK_GROWL         = registerSoundEvent("entity.zippleback.growl");
     public static final RegistryObject<SoundEvent> HIDEOUS_ZIPPLEBACK_HURT          = registerSoundEvent("entity.zippleback.hurt");
@@ -63,6 +67,7 @@ public class ModSounds {
     public static final RegistryObject<SoundEvent> HIDEOUS_ZIPPLEBACK_SLEEP         = registerSoundEvent("entity.zippleback.sleep");
     public static final RegistryObject<SoundEvent> HIDEOUS_ZIPPLEBACK_LIGHTER       = registerSoundEvent("entity.zippleback.lighter");
     public static final RegistryObject<SoundEvent> HIDEOUS_ZIPPLEBACK_POISON_BREATH = registerSoundEvent("entity.zippleback.poison_breath");
+    public static final RegistryObject<SoundEvent> HIDEOUS_ZIPPLEBACK_BITE          = registerSoundEvent("entity.zippleback.bite");
 
     public static final RegistryObject<SoundEvent> TERRIBLE_TERROR_GROWL            = registerSoundEvent("entity.terrible_terror.growl");
     public static final RegistryObject<SoundEvent> TERRIBLE_TERROR_HURT             = registerSoundEvent("entity.terrible_terror.hurt");
@@ -75,12 +80,15 @@ public class ModSounds {
     public static final RegistryObject<SoundEvent> SPEED_STINGER_HURT               = registerSoundEvent("entity.speed_stinger.hurt");
     public static final RegistryObject<SoundEvent> SPEED_STINGER_DEATH              = registerSoundEvent("entity.speed_stinger.death");
     public static final RegistryObject<SoundEvent> SPEED_STINGER_SLEEP              = registerSoundEvent("entity.speed_stinger.sleep");
+    public static final RegistryObject<SoundEvent> SPEED_STINGER_STING              = registerSoundEvent("entity.speed_stinger.sting");
+    public static final RegistryObject<SoundEvent> SPEED_STINGER_BITE               = registerSoundEvent("entity.speed_stinger.bite");
 
     public static final RegistryObject<SoundEvent> STINGER_GROWL                    = registerSoundEvent("entity.stinger.growl");
     public static final RegistryObject<SoundEvent> STINGER_HURT                     = registerSoundEvent("entity.stinger.hurt");
     public static final RegistryObject<SoundEvent> STINGER_DEATH                    = registerSoundEvent("entity.stinger.death");
     public static final RegistryObject<SoundEvent> STINGER_TAME                     = registerSoundEvent("entity.stinger.tame");
     public static final RegistryObject<SoundEvent> STINGER_SLEEP                    = registerSoundEvent("entity.stinger.sleep");
+    public static final RegistryObject<SoundEvent> STINGER_BITE                     = registerSoundEvent("entity.stinger.bite");
 
     private static RegistryObject<SoundEvent> registerSoundEvent(String name) {
         return SOUND_EVENTS.register(name, () -> new SoundEvent(new ResourceLocation(IsleofBerk.MOD_ID, name)));
