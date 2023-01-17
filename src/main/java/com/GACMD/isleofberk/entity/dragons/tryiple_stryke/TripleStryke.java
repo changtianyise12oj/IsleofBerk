f91c8a6ae2d8b401bd54df07aad3a69c354764e8
DevKnight77 <72638153+DevKnight77@users.noreply.github.com>
2023-01-18 05:30:08 +0800

    .

--- src/main/java/com/GACMD/isleofberk/entity/dragons/tryiple_stryke/TripleStryke.java
+++ src/main/java/com/GACMD/isleofberk/entity/dragons/tryiple_stryke/TripleStryke.java
@@ -483,13 +483,13 @@ public class TripleStryke extends ADragonBaseFlyingRideableProjUser {
             ticksSinceLastClawAttack = 0;
         }
 
-        if (random1 > 210) {
+        if (random1 > 120){
             ticksSinceLastStingAttackAI = Util.secondsToTicks(3);
             ticksSinceLastClawAttack = 0;
             ticksSinceLastBiteAttack = 0;
         }
 
-        if (random1 > 250) {
+        if (random1 > 210) {
             ticksSinceLastClawAttack = Util.secondsToTicks(3);
             ticksSinceLastStingAttackAI = 0;
             ticksSinceLastBiteAttack = 0;
