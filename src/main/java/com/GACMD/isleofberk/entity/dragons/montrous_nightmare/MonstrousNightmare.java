fa7467687c2d5bb7f16dde0a7e300e9d30874ddd
DevKnight77 <72638153+DevKnight77@users.noreply.github.com>
2023-01-23 18:47:41 +0800

    .

--- src/main/java/com/GACMD/isleofberk/entity/dragons/montrous_nightmare/MonstrousNightmare.java
+++ src/main/java/com/GACMD/isleofberk/entity/dragons/montrous_nightmare/MonstrousNightmare.java
@@ -272,7 +272,7 @@ public class MonstrousNightmare extends ADragonBaseFlyingRideableBreathUser {
         Vec3 bodyOrigin = position();
 
         double x = -Math.sin(this.getYRot() * Math.PI / 180) * 2.8;
-        double y = getControllingPassenger() != null ? -1.8D : 1D;
+        double y = isFlying() ? -1.8D : 1D;
         double z = Math.cos(this.getYRot() * Math.PI / 180) * 2.8;
         float scale = isBaby() ? 0.2F : 1;
         Vec3 throatPos = bodyOrigin.add(new Vec3(x * scale, y * scale, z * scale));
