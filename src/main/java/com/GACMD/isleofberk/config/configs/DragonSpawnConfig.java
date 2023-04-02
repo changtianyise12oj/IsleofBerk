08564229426c9e822c3a87ce86b21f9b2c726ad0
ApprenticeNecromancer <55693013+ApprenticeNecromancer@users.noreply.github.com>
2023-04-02 20:37:17 +0200

    I want to die
    more skrill files

--- src/main/java/com/GACMD/isleofberk/config/configs/DragonSpawnConfig.java
+++ src/main/java/com/GACMD/isleofberk/config/configs/DragonSpawnConfig.java
@@ -3,6 +3,7 @@ package com.GACMD.isleofberk.config.configs;
 import com.GACMD.isleofberk.config.util.ConfigHelper;
 import com.GACMD.isleofberk.config.util.ConfigHelper.ConfigValueListener;
 import net.minecraftforge.common.ForgeConfigSpec;
+import org.lwjgl.system.CallbackI;
 
 import java.util.List;
 
@@ -31,17 +32,17 @@ public class DragonSpawnConfig
                 value 4: The Biomes to spawn in.
                 If there is a different desired value of weight/counts simply add a new list entry.""".indent(1))
                 .defineList("stinger_data", List.of(
-                        "10 | 1 | 5 | minecraft:swamp, minecraft:forest, minecraft:river",
-                        "12 | 2 | 3 | minecraft:plains, minecraft:ocean"
-                        ), o -> o instanceof String));
+                        "10 | 2 | 4 | minecraft:sunflower_plains, minecraft:savanna, minecraft:savanna_plateau",
+                        "1 | 0 | 3 | minecraft:badlands, minecraft:wooded_badlands, minecraft:eroded_badlands"
+                ), o -> o instanceof String));
         builder.pop();
 
         builder.push("Terrible Terror");
         terribleTerrorData = subscriber.subscribe(builder
                 .comment(" Usage described at the top.")
                 .defineList("terrible_terror_data", List.of(
-                        "20 | 3 | 6 | minecraft:desert",
-                        "12 | 2 | 4 | minecraft:birch_forest"
+                        "1 | 0 | 3 | minecraft:stony_shore, minecraft:river, minecraft:beach",
+                        "4 | 2 | 4 | minecraft:jungle, minecraft:bamboo_jungle, minecraft:dark_forest"
                 ), o -> o instanceof String));
         builder.pop();
 
@@ -49,8 +50,10 @@ public class DragonSpawnConfig
         deadlyNadderData = subscriber.subscribe(builder
                 .comment(" Usage described at the top.")
                 .defineList("deadly_nadder_data", List.of(
-                        "20 | 3 | 6 | minecraft:desert",
-                        "12 | 2 | 4 | minecraft:birch_forest"
+                        "2 | 2 | 3 | minecraft:snowy_plains, minecraft:meadow",
+                        "4 | 2 | 3 | minecraft:windswept_hills, minecraft:windswept_gravelly_hills, minecraft:windswept_forest",
+                        "6 | 2 | 4 | minecraft:sparse_jungle, minecraft:savanna, minecraft:savanna_plateau",
+                        "9 | 1 | 3 | minecraft:grove, minecraft:forest, minecraft:flower_forest, minecraft:birch_forest, minecraft:old_growth_birch_forest, minecraft:windswept_savanna"
                 ), o -> o instanceof String));
         builder.pop();
 
@@ -58,8 +61,9 @@ public class DragonSpawnConfig
         gronckleData = subscriber.subscribe(builder
                 .comment(" Usage described at the top.")
                 .defineList("gronckle_data", List.of(
-                        "20 | 3 | 6 | minecraft:desert",
-                        "12 | 2 | 4 | minecraft:birch_forest"
+                        "2 | 1 | 2 | minecraft:snowy_plains, minecraft:swamp, minecraft:meadow",
+                        "5 | 2 | 3 | minecraft:old_growth_birch_forest, minecraft:savanna_plateau, minecraft:windswept_savanna",
+                        "6 | 1 | 2 | minecraft:plains, minecraft:grove, minecraft:sunflower_plains, minecraft:savanna"
                 ), o -> o instanceof String));
         builder.pop();
 
@@ -67,8 +71,8 @@ public class DragonSpawnConfig
         zipplebackData = subscriber.subscribe(builder
                 .comment(" Usage described at the top.")
                 .defineList("zippleback_data", List.of(
-                        "20 | 3 | 6 | minecraft:desert",
-                        "12 | 2 | 4 | minecraft:birch_forest"
+                        "6 | 2 | 3 | minecraft:sparse_jungle",
+                        "1 | 0 | 1 | minecraft:ice_spikes, minecraft:snowy_plains, minecraft:swamp"
                 ), o -> o instanceof String));
         builder.pop();
 
@@ -76,8 +80,7 @@ public class DragonSpawnConfig
         lightFuryData = subscriber.subscribe(builder
                 .comment(" Usage described at the top.")
                 .defineList("light_fury_data", List.of(
-                        "20 | 3 | 6 | minecraft:desert",
-                        "12 | 2 | 4 | minecraft:birch_forest"
+                        "1 | 0 | 1 | minecraft:jagged_peaks, minecraft:frozen_peaks, minecraft:windswept_forest, minecraft:birch_forest, minecraft:old_growth_birch_forest"
                 ), o -> o instanceof String));
         builder.pop();
 
@@ -85,8 +88,7 @@ public class DragonSpawnConfig
         nightFuryData = subscriber.subscribe(builder
                 .comment(" Usage described at the top.")
                 .defineList("night_fury_data", List.of(
-                        "20 | 3 | 6 | minecraft:desert",
-                        "12 | 2 | 4 | minecraft:birch_forest"
+                        "1 | 0 | 1 | minecraft:snowy_slopes, minecraft:jagged_peaks, minecraft:frozen_peaks, minecraft:windswept_hills, minecraft:windswept_gravelly_hills, minecraft:stony_peaks"
                 ), o -> o instanceof String));
         builder.pop();
 
@@ -94,8 +96,8 @@ public class DragonSpawnConfig
         nightmareData = subscriber.subscribe(builder
                 .comment(" Usage described at the top.")
                 .defineList("nightmare_data", List.of(
-                        "20 | 3 | 6 | minecraft:desert",
-                        "12 | 2 | 4 | minecraft:birch_forest"
+                        "1 | 0 | 1 | minecraft:badlands, minecraft:wooded_badlands, minecraft:eroded_badlands",
+                        "1 | 0 | 1 | minecraft:windswept_hills, minecraft:windswept_gravelly_hills, minecraft:windswept_forest"
                 ), o -> o instanceof String));
         builder.pop();
 
@@ -103,8 +105,7 @@ public class DragonSpawnConfig
         skrillData = subscriber.subscribe(builder
                 .comment(" Usage described at the top.")
                 .defineList("skrill_data", List.of(
-                        "20 | 3 | 6 | minecraft:desert",
-                        "12 | 2 | 4 | minecraft:birch_forest"
+                        "2 | 0 | 1 | minecraft:snowy_slopes, minecraft:jagged_peaks, minecraft:frozen_peaks, minecraft:stony_peaks"
                 ), o -> o instanceof String));
         builder.pop();
 
@@ -112,8 +113,8 @@ public class DragonSpawnConfig
         tripleStrykeData = subscriber.subscribe(builder
                 .comment(" Usage described at the top.")
                 .defineList("triple_stryke_data", List.of(
-                        "20 | 3 | 6 | minecraft:desert",
-                        "12 | 2 | 4 | minecraft:birch_forest"
+                        "1 | 0 | 2 | minecraft:wooded_badlands, minecraft:badlands",
+                        "5 | 1 | 3 | minecraft:snowy_taiga, minecraft:taiga, minecraft:old_growth_pine_taiga, minecraft:old_growth_spruce_taiga"
                 ), o -> o instanceof String));
         builder.pop();
     }
--- src/main/java/com/GACMD/isleofberk/entity/dragons/skrill/SkrillModel.java
+++ src/main/java/com/GACMD/isleofberk/entity/dragons/skrill/SkrillModel.java
@@ -3,9 +3,13 @@ package com.GACMD.isleofberk.entity.dragons.skrill;
 import com.GACMD.isleofberk.IsleofBerk;
 import com.GACMD.isleofberk.entity.base.render.model.BaseDragonModel;
 import com.GACMD.isleofberk.entity.base.render.model.BaseDragonModelFlying;
+import com.GACMD.isleofberk.entity.dragons.nightfury.NightFury;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class SkrillModel extends BaseDragonModelFlying<Skrill> {
 
@@ -24,7 +28,31 @@ public class SkrillModel extends BaseDragonModelFlying<Skrill> {
 
     @Override
     public ResourceLocation getTextureLocation(Skrill entity) {
-        return new ResourceLocation(IsleofBerk.MOD_ID, "textures/dragons/skrill/skrill.png");
+        switch (entity.getDragonVariant()) {
+            default:
+            case 0:
+                return new ResourceLocation(IsleofBerk.MOD_ID,"textures/dragons/skrill/skrill.png");
+            case 1:
+                return new ResourceLocation(IsleofBerk.MOD_ID,"textures/dragons/skrill/skrill_black.png");
+            case 2:
+                return new ResourceLocation(IsleofBerk.MOD_ID,"textures/dragons/skrill/skrill_blue.png");
+            case 3:
+                return new ResourceLocation(IsleofBerk.MOD_ID,"textures/dragons/skrill/skrill_brown.png");
+            case 4:
+                return new ResourceLocation(IsleofBerk.MOD_ID,"textures/dragons/skrill/skrill_crimson.png");
+            case 5:
+                return new ResourceLocation(IsleofBerk.MOD_ID,"textures/dragons/skrill/skrill_cyan.png");
+            case 6:
+                return new ResourceLocation(IsleofBerk.MOD_ID,"textures/dragons/skrill/skrill_gold.png");
+            case 7:
+                return new ResourceLocation(IsleofBerk.MOD_ID,"textures/dragons/skrill/skrill_green.png");
+            case 8:
+                return new ResourceLocation(IsleofBerk.MOD_ID,"textures/dragons/skrill/skrill_red.png");
+            case 9:
+                return new ResourceLocation(IsleofBerk.MOD_ID,"textures/dragons/skrill/skrill_yellow.png");
+            case 10:
+                return new ResourceLocation(IsleofBerk.MOD_ID,"textures/dragons/skrill/skrill_albino.png");
+        }
 
     }
 
@@ -34,7 +62,31 @@ public class SkrillModel extends BaseDragonModelFlying<Skrill> {
     }
 
     @Override
-    public void setLivingAnimations(Skrill entity, Integer uniqueID, AnimationEvent customPredicate) {
-        super.setLivingAnimations(entity, uniqueID, customPredicate);
+    public void setLivingAnimations(Skrill dragon, Integer uniqueID, AnimationEvent customPredicate) {
+        super.setLivingAnimations(dragon, uniqueID, customPredicate);
+
+        // list of bones
+        IBone neck = this.getAnimationProcessor().getBone("Neck");
+        IBone head = this.getAnimationProcessor().getBone("head");
+        IBone spike = this.getAnimationProcessor().getBone("spikeRemove");
+
+        // hide spikes when saddled
+        spike.setHidden(dragon.isSaddled());
+
+        // head tracking
+        EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+
+        if (!dragon.shouldStopMovingIndependently() && !Minecraft.getInstance().isPaused() && !dragon.isRenderedOnGUI()) {
+            neck.setRotationY(neck.getRotationY() + extraData.netHeadYaw * ((float) Math.PI / 180F) / 2);
+            head.setRotationY(head.getRotationY() + extraData.netHeadYaw * ((float) Math.PI / 180F) / 2);
+            neck.setRotationX(neck.getRotationX() + extraData.headPitch * ((float) Math.PI / 180F) / 2);
+            head.setRotationX(head.getRotationX() + extraData.headPitch * ((float) Math.PI / 180F) / 2);
+        }
+    }
+
+
+    @Override
+    public String getMainBodyBone() {
+        return "rotation";
     }
 }
